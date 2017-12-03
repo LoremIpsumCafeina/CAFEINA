36cd50d6f56c74acb57442eb60df1bc3a8b2b74f
Lucas G <lucas.gustavo088@gmail.com>
2017-12-03 10:34:11 -0200

    pessoas snippet

--- resources/views/content/content.blade.php
+++ resources/views/content/content.blade.php
@@ -47,8 +47,26 @@
 					</div>
               </div>
               <div id="tabs-3">
-                <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-                <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+                <div class="card-list-view-users">
+<div class="div-avatar-image">
+<img class="avatar-image" src="https://marketingdeconteudo.com/wp-content/uploads/2017/01/formatos-de-imagem-2.jpg"/>
+</div>
+<div class="text-div">
+  <span>
+Nome do usuário
+</span>
+  <span>
+    São Judas (Mooca)
+    </span>
+    <span>
+    Arquitetura
+    </span>
+</div>
+<div class="icon-mail-div">
+<img src="#" class="icon-mail"/>
+</div>
+</div>
+
               </div>
             </div>
         </div>
